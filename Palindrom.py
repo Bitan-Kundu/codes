d830fcf6416569e9bbf395eed2743b574522b4b8
Bitan-Kundu <91739939+Bitan-Kundu@users.noreply.github.com>
2022-10-11 20:30:51 +0530

    Create Palindrom.py
    Added new code

---
+++ Palindrom.py
@@ -0,0 +1,13 @@
+number = int(input("Enter the number: "))
+temp = number
+reverse = 0
+
+while(number>0):
+    dig = number%10
+    reverse = reverse * 10 + dig
+    number = number//10
+    
+if(temp==reverse):
+    print("The number is palindrom")
+else:
+    print("The number is not palindrom")
